8643067615909cdca1ffda617468c6715cceeff3
sefufim <132181028+sefufim@users.noreply.github.com>
2024-01-08 19:43:24 -0800

    remove the head

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,12 @@ import About from "@/components/sections/about";
 import Footer from "@/components/Footer/footer";
 import ProjectDisplay from "@/components/sections/projects";
 import Contact from "@/components/sections/contact";
-import Head from "next/head";
+
 
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Your SEO Title Here</title>
-      </Head>
       <main>
         <Hero/> 
         <About/>
