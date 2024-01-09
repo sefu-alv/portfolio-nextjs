e571b4ebad0870cc9011b0a96c04d39c7b24bedb
sefufim <132181028+sefufim@users.noreply.github.com>
2024-01-08 19:38:47 -0800

    added the head

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,21 @@ import About from "@/components/sections/about";
 import Footer from "@/components/Footer/footer";
 import ProjectDisplay from "@/components/sections/projects";
 import Contact from "@/components/sections/contact";
+import Head from "next/head";
 
 
 export default function Home() {
   return (
-    <main>
-      <Hero/> 
-      <About/>
-      <ProjectDisplay/>
-
-      <Contact/>
-    </main>
+    <>
+      <Head>
+        <title>Your SEO Title Here</title>
+      </Head>
+      <main>
+        <Hero/> 
+        <About/>
+        <ProjectDisplay/>
+        <Contact/>
+      </main>
+    </>
   )
 }
