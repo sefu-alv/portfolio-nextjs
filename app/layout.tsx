4c94cf08d36e18153e5d23661891a6d3c98850b1
sefufim <132181028+sefufim@users.noreply.github.com>
2024-01-08 19:26:12 -0800

    added new meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
       <Head>
         <title>Sefufim</title>
         <meta name="keyword" content="portfolio, typescript, developer, react, nextjs" />
+        <meta name="google-site-verification" content="Z94gNptf5Vpn91g8H3nlkGf_DZXKZORD-HY52DTVqn0" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
