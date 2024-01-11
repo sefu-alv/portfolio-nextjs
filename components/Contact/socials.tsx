1d3f5f2d612314efc567d61c8b0c9bcb599d44f9
Sefufim Alvarez <sefufimalvarez@Sefufims-Laptop.local>
2024-01-10 18:50:54 -0800

    fixed the apos

--- components/Contact/socials.tsx
+++ components/Contact/socials.tsx
@@ -5,7 +5,7 @@ export default function Socials () {
         <div className="min-h-screen flex flex-col items-center justify-center gap-16 my-6 w-[35%]">
             <div>
                     <h1 className="text-5xl font-extrabold">Let&apos;s Talk</h1>
-                    <p className="mt-3">Got any ideas or brand you want developed? If so I'd love to hear about it. </p>
+                    <p className="mt-3">Got any ideas or brand you want developed? If so I&apos;d love to hear about it. </p>
             </div>
            
             <div className="w-full text-4xl font-bold">
